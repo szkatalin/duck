424cdeb9255c31abeba322d5143972c9819f077a
mate <tajtimt@gmail.com>
2018-12-09 16:57:13 +0100

    Revert "Revert "Mate-enemies""
    This reverts commit 19e34105a0083ece73edd49738bcb40ee8acb085.

--- Duck Game/Assets/Scripts/MovingObject.cs
+++ Duck Game/Assets/Scripts/MovingObject.cs
@@ -15,7 +15,6 @@ public class MovingObject : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         currentTarget = endPointOfMoving.position;
-
 	}
 	
 	// Update is called once per frame
